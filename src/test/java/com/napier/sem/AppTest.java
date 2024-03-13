ddbbf45022c04e847cc61d384e03aed95036f2e3
oliwia-j <olivska@gmail.com>
2024-03-13 13:02:49 +0000

    Set up Unit Testing.

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -1,43 +1,53 @@
 package com.napier.sem;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.testng.annotations.Test;
-
-import javax.xml.crypto.Data;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest
 {
+    static App app;
     static DataHolder dataHolder;
 
     @BeforeAll
     static void init()
     {
+        app = new App();
         DataHolder dataHolder = new DataHolder();
     }
 
     @Test
     void countriesByPopTest()
     {
-        dataHolder.countriesByPop();
+
+        //dataHolder.countriesByPop();
     }
+    @Test
     void countriesByPopInContinentTest()
     {
-        dataHolder.countriesByPopInContinent(null);
+
+        //dataHolder.countriesByPopInContinent(null);
     }
+    @Test
     void countriesByPopInRegionTest()
     {
-        dataHolder.countriesByPopInRegion(null);
+
+        //dataHolder.countriesByPopInRegion(null);
     }
+    @Test
     void NcountriesByPopTest()
     {
-        dataHolder.NcountriesByPop(null);
+
+        //dataHolder.NcountriesByPop(null);
     }
+    @Test
     void NcountriesByPopInContinentTest()
     {
-        dataHolder.NcountriesByPopInContinent(null,null);
+        //dataHolder.NcountriesByPopInContinent(null,null);
     }
+    @Test
     void NcountriesByPopInRegionTest()
     {
-        dataHolder.NcountriesByPopInRegion(null,null);
+
+        //dataHolder.NcountriesByPopInRegion(null,null);
     }
 }
